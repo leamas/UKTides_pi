3ce85c93f116afba2f5dfcd7e32e3988cf6393ae
Rasbats <miker@members.swis.net>
2020-12-07 19:22:00 +0000

    v1.0.145

--- ci/circleci-build-flatpak.sh
+++ ci/circleci-build-flatpak.sh
@@ -29,16 +29,16 @@ flatpak remote-add --user --if-not-exists flathub \
     https://flathub.org/repo/flathub.flatpakrepo
 flatpak install --user -y flathub org.opencpn.OpenCPN > /dev/null
 flatpak install --user -y flathub org.freedesktop.Sdk//18.08  >/dev/null
-cp flatpak/org.opencpn.OpenCPN.Plugin.uktides.yaml flatpak.yaml.bak
+cp flatpak/org.opencpn.OpenCPN.Plugin.UKTides.yaml flatpak.yaml.bak
 sed -i '/^runtime-version/s/:.*/: stable/' \
-    flatpak/org.opencpn.OpenCPN.Plugin.uktides.yaml
+    flatpak/org.opencpn.OpenCPN.Plugin.UKTides.yaml
 
 mkdir build; cd build
 cmake  ..
 make flatpak
 
 # Restore file so the cache checksumming is ok.
-cp ../flatpak.yaml.bak org.opencpn.OpenCPN.Plugin.uktides.yaml
+cp ../flatpak.yaml.bak org.opencpn.OpenCPN.Plugin.UKTides.yaml
 
 # Wait for apt-daily to complete, install cloudsmith-cli required by upload.sh.
 # apt-daily should not restart, it's masked.
